d311388a5f6916c8e786ad7430c663f12fbd2067
Kevin GOUOMBA MANGA <kevin.gouombamanga@epfedu.fr>
2020-03-26 16:19:59 +0100

    We created the admin dashboard, with the 2 differents lists

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 
 
 # Create your views here.
 # For HTTP requests
 
-def index(response, id):
-    return HttpResponse("<h1>%s</h1>" % id)
+def index(response, name):  # possible with id
+    ls = ToDoList.objects.get(name=name)
+    item = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" % (ls.name, str(item.text)))

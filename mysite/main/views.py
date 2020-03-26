5fd812fc6aa3b759d65a195e2b7ecdf4ad2601fd
Kevin GOUOMBA MANGA <kevin.gouombamanga@epfedu.fr>
2020-03-26 14:20:04 +0100

    Setup, installation and page navigation

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import Httpresponse
+
 
 # Create your views here.
+# For HTTP requests
+
+def index(response):
+    return Httpresponse("<h1>Test Djange Database<\h1>")

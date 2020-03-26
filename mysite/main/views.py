9f6b0eb150dad866abe5327c62c925f5a1d4b5ec
Kevin GOUOMBA MANGA <kevin.gouombamanga@epfedu.fr>
2020-03-26 14:27:05 +0100

    Correction of Response

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.http import Httpresponse
+from django.http import HttpResponse
 
 
 # Create your views here.
 # For HTTP requests
 
 def index(response):
-    return Httpresponse("<h1>Test Djange Database<\h1>")
+    return HttpResponse("<h1>Test Djange Database<\h1>")
+
+
+def v1(response):
+    return HttpResponse("<h1>View 1<\h1>")
